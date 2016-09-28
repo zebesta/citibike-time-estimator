5a125e53000f89d48f197bfafb6a9ceab78e00a7
zebesta <chris.sebesta@gmail.com>
2016-09-28 12:20:10 -0400

    working with my hard coded commute

--- index.js
+++ index.js
@@ -7,18 +7,46 @@ var distance = require('google-distance-matrix');
 distance.key('AIzaSyDmbMZu5BBQ9i3bH5ZJXXMeXnIiAmh9C9c');
 
 var origins = ['70 Maujer, Brooklyn'];
+var leonardStId = 3079;
+var howardStId = 268;
 var destinations = ['455 Broadway, New York'];
+var totalTime = 0;
 
 // console.log(citibike);
 // console.log(stations);
 var leonardStation = stations.stationBeanList.filter(function( obj ) {
-  return obj.id == 3079;
+  return obj.id == leonardStId;
 });
 var leonardStationLocation = [''+leonardStation[0].latitude + ', '+ leonardStation[0].longitude];
+
+var howardStation = stations.stationBeanList.filter(function( obj ) {
+  return obj.id == howardStId;
+});
+var howardStationLocation = [''+howardStation[0].latitude + ', '+ howardStation[0].longitude];
+
 distance.mode('walking');
 distance.matrix(origins, leonardStationLocation, function (err, distances) {
     if (!err)
+        console.log("Firat walk distance: ");
         console.log(distances.rows[0].elements);
+        totalTime += distances.rows[0].elements[0].duration.value;
 })
 
-console.log(leonardStation);
+distance.mode('bicycling');
+distance.matrix(leonardStationLocation, howardStationLocation, function (err, distances) {
+    if (!err)
+        console.log("First bike distance: ")
+        console.log(distances.rows[0].elements);
+        totalTime += distances.rows[0].elements[0].duration.value;
+})
+
+distance.mode('walking');
+distance.matrix(howardStationLocation, destinations, function (err, distances) {
+    if (!err)
+        console.log("Second bike distance: ")
+        console.log(distances.rows[0].elements);
+        totalTime += distances.rows[0].elements[0].duration.value;
+        console.log("Total time is " + Math.floor(totalTime/60) + " minutes and " +  totalTime%60 + " seconds");
+})
+
+// console.log(leonardStation);
