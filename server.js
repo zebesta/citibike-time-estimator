84b12f363cf94e41892e0e38eb2172576992b43f
zebesta <chris.sebesta@gmail.com>
2016-10-11 11:55:28 -0400

    requiring CORS package

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 var express = require('express');
 var geo = require('./geocodeAddresses');
 var calculate = require('./calculateDistanceMatrices');
+var cors = require('cors')
 
 var app = express();
 
-app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // next();
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+app.use(cors());
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
